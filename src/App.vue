a50b9ee91e8d1f410dfcef42a10b9c4d5306c09d
PaoloIncollu <paoloinco@gmail.com>
2025-01-09 18:47:46 +0100

    caricamento progetto

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+
+    import HomeMain from './components/Homepage/HomeMain.vue';
+    import HomeFooter from './pages/HomeFooter.vue';
+
+    export default {
+        data() {
+            return { 
+
+            }
+        },
+
+        components: {
+            HomeMain,
+            HomeFooter
+        },
+        methods: {
+
+        }
+    }
+
+</script>
+
+<template>
+
+    <div>
+        <HomeMain />
+        <HomeFooter />
+    </div>
+
+</template>
+
+<style lang="scss">
+    @use 'assets/scss/main' as *;
+    @import "bootstrap/scss/bootstrap";
+</style>
